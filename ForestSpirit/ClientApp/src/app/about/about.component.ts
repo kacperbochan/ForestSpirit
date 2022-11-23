1089163a74613f3a87a288abd640c5db99b69201
krzysiek <krzysioszw@gmail.com>
2022-11-24 00:46:50 +0100

    about test

--- ForestSpirit/ClientApp/src/app/about/about.component.ts
+++ ForestSpirit/ClientApp/src/app/about/about.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    document.body.style.backgroundImage = "url('../../assets/foggy-pine-forest-background-vector.jpg)";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundSize = "cover"; 
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnExit(){
+    document.body.style.backgroundImage = "none";
   }
 
 }
